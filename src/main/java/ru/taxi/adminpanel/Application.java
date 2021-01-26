5175498c6cb7551ee23a9f05bfb7affb777b97f5
kezik <kezikov@vtb.ru>
2021-01-27 00:26:48 +0300

    -

--- src/main/java/ru/taxi/adminpanel/Application.java
+++ src/main/java/ru/taxi/adminpanel/Application.java
@@ -13,7 +13,6 @@ public class Application extends SpringBootServletInitializer implements AppShel
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-
     }
 
 }
