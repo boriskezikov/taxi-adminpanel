aeca83db84497a65292df18babcdf16cfa19aa6a
kezik <kezikov@vtb.ru>
2021-01-27 08:04:38 +0300

    test

--- src/main/java/ru/taxi/adminpanel/backend/StarterConfig.java
+++ src/main/java/ru/taxi/adminpanel/backend/StarterConfig.java
@@ -7,12 +7,5 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class StarterConfig {
 
-    @Bean
-    public WebServerFactoryCustomizer containerCustomizer() {
-        return (container -> {
-            container.set("/nemswiftsvc");
-            container.setPort(Integer.valueOf(System.getenv("PORT")));
-        });
-    }
 
 }
